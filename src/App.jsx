fe36da657ed47ff1e66aaa50c9272dabc986e876
Dev-Narpheesah <159795729+Dev-Narpheesah@users.noreply.github.com>
2024-06-11 15:33:11 +0100

    Contact Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contact from "./Contact"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header/>
     <Footer/>
+    <Contact/>
     
     </>
   )
---
+++ src/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <h2 style={{color: "blue"}}>Contact Us</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
