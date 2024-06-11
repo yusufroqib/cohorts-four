ca22defcc026af8baca181119d8b2fdd57088c06
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-06-11 15:34:07 +0100

    update-hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Hero />
     <Footer/>
     
     </>
