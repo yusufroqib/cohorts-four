1c46c4a7b924c4e1dba7f0ef6d73b952324f0cfc
muritalazainab <muritala21@icloud.com>
2024-06-11 15:34:11 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Team from "./Team"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Team/>
     <Footer/>
     
     </>
---
+++ src/Team.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './team.css'
+
+const Team = () => {
+  return (
+    <div className='team'>
+      <h1>Teamwork collaboration</h1> 
+      <p>
+        We are a team of highly skilled and experienced professionals </p>
+    </div>
+  )
+}
+
+export default Team
